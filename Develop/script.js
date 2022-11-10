29325c37e906f92a2e5601879797e55556095ee4
Tanner Peacock <thejamespeacock2020@gmail.com>
2022-11-10 01:04:58 -0500

    added extra  comments where to explain what i was doing if i ever wanted to come back and work some more

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,4 @@
-// Assignment Code
+// variables in the global scope so they can be called anywhere in the javascript
 var generateBtn = document.querySelector("#generate");
 var numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var symbolList = ["!", "#", "$", "%", "^", "&", "*"];
@@ -43,6 +43,7 @@ function generatePassword() {
   //This if statement lets us know that if the User Inputed password length is not a number we will get an alert saying that it is not a number
   if (isNaN(userInput)) {
     window.alert("That's not a number!");
+    //
     generatePassword();
   }
 
@@ -51,22 +52,28 @@ function generatePassword() {
     window.alert("Password Length Must Be Between 8 and 128 characters");
     generatePassword();
   } else {
+    //confirms if user wants numbers in their password
+    // concat is used to combine two or more arrays
     if (confirm("Would you like to include numbers in your password?")) {
       optionsCart = optionsCart.concat(numberList);
     }
+    //confirms if user wants uppercase letters in their password
     if (
       confirm("Would you like to include uppercase letters in your password?")
     ) {
       optionsCart = optionsCart.concat(uppercaseList);
     }
+    //confirms if user wants lower letters in their password
     if (
       confirm("Would you like to include lowercase letters in your password?")
     ) {
       optionsCart = optionsCart.concat(lowercaseList);
     }
+    //confirms if user wants symbols in their password
     if (confirm("Would you like to include symbols in your password?")) {
       optionsCart = optionsCart.concat(symbolList);
     }
+    //lets user know that if they didnt pick any options they cant generate a password and runs the generate password function again so they can do it again
     if (optionsCart.length === 0) {
       alert(
         "You must choose atleast one type of charcater to generate a password"
