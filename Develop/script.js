6e220bbd713622aab1e91d424799b7203ef541bd
Tanner Peacock <thejamespeacock2020@gmail.com>
2022-11-10 00:55:32 -0500

    added variables and for loop to change lowercase list to uppercase when it is needed

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,45 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var symbolList = ["!", "#", "$", "%", "^", "&", "*"];
+  var lowercaseList = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  var uppercaseList = [];
+  for (var i = 0; i < lowercaseList.length; i++) {
+    uppercaseList[i] = lowercaseList[i].toUpperCase();
+  }
 
+
+function generatePassword() {
+  var generatedPassword = "";
+  var optionsCart = [];
+  var userInput = window.prompt("How long do you want your password to be?");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
