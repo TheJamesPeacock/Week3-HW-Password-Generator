85cd13966506239ad198c542bc272f2e0840f2d8
Tanner Peacock <thejamespeacock2020@gmail.com>
2022-11-10 00:57:24 -0500

    added some if statements like (if userinput is not a number they will get an alert letting them know it is not a number

--- Develop/script.js
+++ Develop/script.js
@@ -40,6 +40,21 @@ function generatePassword() {
   var generatedPassword = "";
   var optionsCart = [];
   var userInput = window.prompt("How long do you want your password to be?");
+
+  //This if statement lets us know that if the User Inputed password length is not a number we will get an alert saying that it is not a number
+ if (isNaN(userInput)) {
+  window.alert("That's not a number!");
+  generatePassword()
+}
+
+// Lets us know that if the password entered is less than 8 or greater than 128 characters it is invalid.
+if (userInput < 8 || userInput > 128) {
+  window.alert("Password Length Must Be Between 8 and 128 characters");
+  generatePassword()
+} else {
+  if(confirm("Would you like to include numbers in your password?")) {
+    optionsCart = optionsCart.concat(numberList)
+  }
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
